bf1b318abd269ed4371cd5be9c8371b81931aaa9
Genki Furukawa <genki.furukawa@gmail.com>
2019-04-29 09:59:13 +0900

    [update] 8章の内容を写経

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,12 +1,22 @@
 package money;
 
-public class Money {
+abstract public class Money {
 
 	protected int amount;
+
+	abstract Money times(int multiplier);
 
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return this.amount == money.amount && this.getClass() == money.getClass();
 	}
 
+	//	staticメソッドは、newを使わずに呼び出すことができる
+	public static Dollar dollar(int amount) {
+		return new Dollar(amount);
+	}
+
+	public static Franc franc(int amount) {
+		return new Franc(amount);
+	}
 }
