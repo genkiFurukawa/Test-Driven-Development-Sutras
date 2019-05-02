efda65e46fad92114a7aa477200822bb71caa5be
Genki Furukawa <genki.furukawa@gmail.com>
2019-05-02 22:28:05 +0900

    [update] 18章の内容を写経
    - WasRunクラスが2つの仕事(テストが起動されたか記録する仕事、テストメソッドを動的に呼び出す仕事)を担うようになったので分離

--- 02/xunit.py
+++ 02/xunit.py
@@ -1,17 +1,36 @@
-class WasRun:
+class TestCase:
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
     
     def run(self):
         # getattr:objectの指定された属性を取得する
         method = getattr(self, self.name)
-        method()
+        method()        
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        # self.name = name
+        super().__init__(name)
+    
+    # def run(self):
+    #     # getattr:objectの指定された属性を取得する
+    #     method = getattr(self, self.name)
+    #     method()
 
     def testMethod(self):
         self.wasRun = 1
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunnig(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+
+TestCaseTest("testRunnig").run()
+# test = WasRun("testMethod")
+# print(test.wasRun)
+# test.run()
+# print(test.wasRun)
