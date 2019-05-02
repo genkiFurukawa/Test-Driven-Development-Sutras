56b716e8cc517dc55563cf443e657a21b251e52e
Genki Furukawa <genki.furukawa@gmail.com>
2019-05-02 23:36:25 +0900

    [update] 21章の内容を写経

--- 02/xunit.py
+++ 02/xunit.py
@@ -1,3 +1,13 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -9,11 +19,14 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         # getattr:objectの指定された属性を取得する
         method = getattr(self, self.name)
         method
         self.tearDown()
+        return result
     
 class WasRun(TestCase):
     def setUp(self):
@@ -21,6 +34,9 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.log = self.log + "testMethod "
+
+    def testBrokenMethod(self):
+        raise Exception
 
     def tearDown(self):
         self.log = self.log + "tearDown "
@@ -30,5 +46,17 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown" == self.test.log)
+    
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
