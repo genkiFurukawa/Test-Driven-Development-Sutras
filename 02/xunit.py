c4979b3507023c58abbd1af68138fe0ae868d508
Genki Furukawa <genki.furukawa@gmail.com>
2019-05-02 22:51:43 +0900

    [update] 19章の内容を写経

--- 02/xunit.py
+++ 02/xunit.py
@@ -1,17 +1,25 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
-    
+
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         # getattr:objectの指定された属性を取得する
         method = getattr(self, self.name)
-        method()        
+        method
 
 class WasRun(TestCase):
-    def __init__(self, name):
+    # def __init__(self, name):
+    #     self.wasRun = None
+    #     # self.name = name
+    #     super().__init__(name)
+    
+    def setUp(self):
         self.wasRun = None
-        # self.name = name
-        super().__init__(name)
+        self.wasSetUp = 1
     
     # def run(self):
     #     # getattr:objectの指定された属性を取得する
@@ -22,14 +30,23 @@ class WasRun(TestCase):
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunnig(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        # test = WasRun("testMethod")
+        # assert(not test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    
+    def testSetUp():
+        # test = WasRun("testMethod")
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 
 TestCaseTest("testRunnig").run()
+TestCaseTest("testSetUp").run()
 # test = WasRun("testMethod")
 # print(test.wasRun)
 # test.run()
