f224cbe5fb64224d046118579971b1c38bc6f62e
Genki Furukawa <genki.furukawa@gmail.com>
2019-05-02 23:11:11 +0900

    [update] 20章の内容を写経

--- 02/xunit.py
+++ 02/xunit.py
@@ -5,12 +5,16 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         # getattr:objectの指定された属性を取得する
         method = getattr(self, self.name)
         method
-
+        self.tearDown()
+    
 class WasRun(TestCase):
     # def __init__(self, name):
     #     self.wasRun = None
@@ -18,8 +22,9 @@ class WasRun(TestCase):
     #     super().__init__(name)
     
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        # self.wasRun = None
+        # self.wasSetUp = 1
+        self.log = "setUp "
     
     # def run(self):
     #     # getattr:objectの指定された属性を取得する
@@ -27,25 +32,35 @@ class WasRun(TestCase):
     #     method()
 
     def testMethod(self):
-        self.wasRun = 1
+        # self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunnig(self):
-        # test = WasRun("testMethod")
-        # assert(not test.wasRun)
-        self.test.run()
-        assert(self.test.wasRun)
+    # def testRunnig(self):
+    #     # test = WasRun("testMethod")
+    #     # assert(not test.wasRun)
+    #     self.test.run()
+    #     assert(self.test.wasRun)
+    
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == self.test.log)
     
     def testSetUp():
         # test = WasRun("testMethod")
         self.test.run()
-        assert(self.test.wasSetUp)
+        assert("setUp testMethod " == self.test.log)
 
 
-TestCaseTest("testRunnig").run()
+# TestCaseTest("testRunnig").run()
+TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testSetUp").run()
 # test = WasRun("testMethod")
 # print(test.wasRun)
